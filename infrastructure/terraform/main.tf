39cf5ef2de6009454130f3903ac76916e6067731
Kadir Barut <kadirrbrtt@gmail.com>
2026-09-05 04:23:59 +0300

    Allow BuildKit ECR manifest reads

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -229,6 +229,7 @@ resource "aws_iam_role_policy" "github_actions_ecr" {
         Effect = "Allow"
         Action = [
           "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
           "ecr:CompleteLayerUpload",
           "ecr:DescribeImages",
           "ecr:InitiateLayerUpload",
